5a7de836eb68b64782e503467e34e82bda9333e2
beninsydney <benodev@protonmail.com>
2022-08-01 12:22:44 +1000

    add icons

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,17 +1,33 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-/* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
-
-/* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-/* import specific icons */
-import { faHouse, faTag, faListCheck, faUser, faMoneyBillTransfer, faSackDollar, faIdCard, faSignature} from '@fortawesome/free-solid-svg-icons'
+/* side buttons for application progress */
+import { 
+  faHouse, 
+  faTag, 
+  faListCheck, 
+  faUser, 
+  faMoneyBillTransfer, 
+  faSackDollar, 
+  faIdCard, 
+  faSignature
+} from '@fortawesome/free-solid-svg-icons'
 
-/* add icons to the library */
-library.add(faHouse, faTag, faListCheck, faUser, faMoneyBillTransfer, faSackDollar, faIdCard, faSignature)
+/* property type buttons */
+import {
+  faTree,
+  faHouseChimney,
+  faHouseFloodWater,
+  faShop,
+  faChalkboard
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  faHouse, faTag, faListCheck, faUser, faMoneyBillTransfer, faSackDollar, faIdCard, faSignature,
+  faTree, faHouseChimney, faHouseFloodWater, faShop, faChalkboard
+)
 
 createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
